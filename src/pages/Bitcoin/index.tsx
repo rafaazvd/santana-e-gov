1a796b9c19c635c8913703cd3efe88d51d850938
rafaazvd <rafa-spdf@hotmail.com>
2020-08-13 15:40:22 -0300

    page bitcoin

--- src/pages/Bitcoin/index.tsx
+++ src/pages/Bitcoin/index.tsx
@@ -14,8 +14,35 @@ const Bitcoin: React.FC = () => {
                 <Img src={bitcoin2} alt="e-gov"/>
             </Header>
             <Section>
-                <h1>Title</h1>
-                <p>conteudo</p>
+                <h1>VANTAGENS DE SE USAR BITCOIN OU OUTRA MOEDA DIGITAL</h1>
+                <h2>Transparência</h2>
+                <p>Todas as informações de transações são criptografadas
+                 é um livro contábil público e imutável. </p>
+
+                <h2>Um Dinheiro Internacional </h2>
+                <p>Uma moeda universal, de fácil transferência e 
+                    sem taxas de câmbio que pode ser usada em 
+                    transações internacionais com facilidade.</p>
+
+                <h2>Protege Contra a Inflação</h2>
+                <p>O Programador que criou o Bitcoin definiu nos 
+                protocolos um limite de 21 milhões de unidades o que
+                 torna o sistema Bitcoin deflacionário por natureza, 
+                 já que atingido esse limite o próprio software vai
+                  impedir a geração de novas emissões.
+                Segundo o site <a href='https://www.infomoney.com.br/colunista/terraco-economico/uma-sociedade-sem-inflacao-com-bitcoin-seria-possivel' >InfoMoney</a> O Bank of England elaborou um estudo que teve grande 
+                 repercussão: modelou uma economia em que a moeda digital 
+                 seria equivalente a 30% do PIB, os resultados indicaram
+                  um aumento de 3% no PIB anual, explicados pela queda
+                   substancial dos custos de transações, nas taxas de
+                    juros e ganhos de eficiência.</p>
+
+                <h2>Um Dinheiro Protegido do Estado</h2>
+                <p>Por ser uma uma moeda digital, o governo
+                     ou instituições financeiras não podem 
+                     regular, congelar ou controlar o seu 
+                     dinheiro, ou seja, você é o seu próprio 
+                     banco e tem total controle sobre ele.</p>
             </Section>
             <br />
             <Footer />
--- src/pages/Bitcoin/styles.ts
+++ src/pages/Bitcoin/styles.ts
@@ -1,14 +1,58 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    
+    section {
+        h1 {
+            margin-top: 3%;
+            margin-bottom: 4%;
+            text-align: center;
+            background-color: #6D7071;
+            width: 70%;
+            margin-left: 17%;
+            font-size: 20px;
+            padding: 2%;
+            color: #fff;
+            border-radius: 11px;
+            @media(max-width: 800px){
+                font-size: 10px;
+                padding: 5%;
+            }
+        }
+        }
+        h2 {
+            margin: 2%;
+            text-align: center;
+            background-color: #7CAFC4;
+            font-size: 19px;
+            padding: 2%;
+            color: #3C3F40;
+            border-radius: 11px;
+            @media(max-width: 800px){
+                font-size: 10px;
+            }
+        }
+        p {
+            text-align: justify;
+            width: 50%;
+            margin-left: 25%;
+            margin-top: 2%;
+            @media(max-width: 800px){
+                font-size: 10px;
+                margin-left: 3%;
+                width: 90%;
+            }
+        }
+    }
 `;
 
 export const Text = styled.h1`
     color: #F1F5F7;
     margin: 8%;
-    font-size: 47px;
+    font-size: 67px;
+    @media(max-width: 800px){
+        font-size: 27px;
+    }
 `;
 export const Img = styled.img`
-    width: 16%;
+    width: 14%;
 `; 
